7fddcbeb2450289025f89f31362aa87906cd491b
anjmao <andzej.maciusovic@gmail.com>
2019-07-18 14:10:43 +0300

    Update goroute

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/goroute/examples
+
+go 1.12
+
+require github.com/goroute/compress v0.0.0-20190718072600-b7725fb2d488 // indirect
--- hello/main.go
+++ hello/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
-	"github.com/goroute/route"
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/goroute/route"
 )
+
+func logger() route.MiddlewareFunc {
+	return func(c route.Context, next route.HandlerFunc) error {
+		fmt.Println("Request Path:", c.Request().URL.Path)
+		return next(c)
+	}
+}
 
 func main() {
 	mux := route.NewServeMux()
+
+	mux.Use(logger())
 
 	mux.GET("/", func(c route.Context) error {
 		return c.String(http.StatusOK, "Hello!")
