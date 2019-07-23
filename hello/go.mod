22a20a25660d0e97b65aa0a6d384e8622121b627
anjmao <andzej.maciusovic@gmail.com>
2019-07-23 09:53:39 +0300

    Add rest api example

--- hello/go.mod
+++ hello/go.mod
@@ -2,4 +2,7 @@ module do
 
 go 1.12
 
-require github.com/goroute/route v0.0.0-20190711182439-6728791eeef1
+require (
+	github.com/goroute/route v0.0.0-20190719070023-d954e0fc21fb
+	github.com/stretchr/objx v0.2.0 // indirect
+)
--- hello/main.go
+++ hello/main.go
@@ -1,24 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/goroute/route"
 )
 
-func logger() route.MiddlewareFunc {
-	return func(c route.Context, next route.HandlerFunc) error {
-		fmt.Println("Request Path:", c.Request().URL.Path)
-		return next(c)
-	}
-}
 
 func main() {
 	mux := route.NewServeMux()
 
-	mux.Use(logger())
+	mux.Use(func(c route.Context, next route.HandlerFunc) error {
+		log.Println(c.Request().URL.Path)
+		return next(c)
+	})
 
 	mux.GET("/", func(c route.Context) error {
 		return c.String(http.StatusOK, "Hello!")
